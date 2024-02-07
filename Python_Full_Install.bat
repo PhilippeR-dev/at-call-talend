9adefb4d7a1344ff0d7629a957e0d5bb28dcf63c
Philippe REY <prey@synergy.fr>
2024-02-07 19:18:36 +0100

    modif install

--- Python_Full_Install.bat
+++ Python_Full_Install.bat
@@ -8,10 +8,13 @@ set PythonPath=%cd%\python
 echo PythonPath = %PythonPath%
 :: Installation des Packages en pré-requis
 mkdir %PythonPath%
+:: Décompression du fichier python-3.11.7-amd64.zip
+cd /d %InstallPath%
+powershell -command Expand-Archive -Path python-3.11.7-amd64.zip -DestinationPath %PythonPath%
 :: Installation de l interpreteur Python
 echo Debut Installation Python interpreteur
 ::ren %InstallPath%\python-3.11.7-amd64.exe.txt python-3.11.7-amd64.exe
-%InstallPath%\python-3.11.7-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 TargetDir=%PythonPath%\
+%PythonPath%\python-3.11.7-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 TargetDir=%PythonPath%\
 echo Fin Installation Python interpreteur
 
 :: Installation des Packages en pré-requis
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# at-call-talend
+# Lanceur de batchs Talend
+
+## Installation de python et ses dépendances
+
+>  Exécuter le fichier "Python_Full_Install.bat" en tant qu'administrateur
+
+## Exécution du lanceur de task Talend par un ordonnanceur
+
+* Modifier les variables d'environnements
+* Renseigner le numéro de la Task Talend
+* Exécuter le fichier "call_talend_launcher.bat" en tant qu'administrateur
+
+## Exécution du lanceur de task Talend en mode test
+
+* Modifier les variables d'environnements
+* Renseigner le numéro de la Task Talend
+* Exécuter le fichier "call_talend_launcher_test.bat" en tant qu'administrateur
